546c571379f86c4f9e6468641e9ddc7a4f07ec27
facundocarabajal <facundocarabajal10@gmail.com>
2018-11-06 19:42:08 -0300

    ADD: BD and modified Match and Tournament

--- DataLayer/Match.py
+++ DataLayer/Match.py
@@ -1,6 +1,7 @@
 class Match:
-    def __init__(self, id, team_1, team_2, winner_team):
+    def __init__(self, id, team_1, team_2, winner_team, id_tournament):
         self.id = id
         self.team_1 = team_1
         self.team_2 = team_2
         self.winner_team = winner_team
+        self.id_tournament = id_tournament
