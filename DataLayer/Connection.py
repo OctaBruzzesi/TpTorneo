bfe0a6a9ee6a898178bf3dcb4cce3295a549d537
Octa Bruzzesi <octa.bruzzesi@gmail.com>
2018-11-12 02:07:48 -0300

    FIX: config

--- DataLayer/Connection.py
+++ DataLayer/Connection.py
@@ -1,12 +1,14 @@
 import pymysql
 
+
 class Connection:
     def __init__(self):
         try:
-            self.conn = pymysql.connect(host='127.0.0.1',
-                             user='root',
-                             password='root',
-                             db='tournaments')
+            self.conn = pymysql.connect(
+                host='127.0.0.1',
+                user='root',
+                password='root',
+                db='tournaments')
             self.cur = self.conn.cursor()
         except Exception as f:
             print("Error al ejecutar SQL " + str(f))
@@ -14,6 +16,7 @@ class Connection:
     def execute(self, query):
         self.cur.execute(query)
         self.conn.commit()
+
 
 if __name__ == '__main__':
     con = Connection()
