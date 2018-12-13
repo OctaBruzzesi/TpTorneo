df7e051375f00f69c823e98f1bbc7a9238cfdc52
Octa Bruzzesi <octa.bruzzesi@gmail.com>
2018-12-13 00:10:30 -0300

    WIP: menu styles

--- UserLayer/Menu.py
+++ UserLayer/Menu.py
@@ -9,16 +9,24 @@ class Menu:
 
         self.window = Tk()
 
-        label = ttk.Label(self.window, text="Start Page")
+        title_frame = Frame(self.window, width=150)
+        title_frame.grid(row=0, column=0, columnspan=3)
+        title_frame.configure(background="#4FC1E9")
+
+        label = ttk.Label(title_frame, text="Tournaments")
         label.pack(pady=10, padx=125)
 
-        button = ttk.Button(self.window, text="Lista de Torneos", command=self.open_tournament)
+        button_frame = Frame(self.window, width=150)
+        button_frame.grid(row=1, column=0, columnspan=3)
+        button_frame.configure(background="#3BAFDA")
+
+        button = ttk.Button(button_frame, text="Lista de Torneos", command=self.open_tournament)
         button.pack()
 
-        button2 = ttk.Button(self.window, text="Lista de Equipos", command=self.open_team)
+        button2 = ttk.Button(button_frame, text="Lista de Equipos", command=self.open_team)
         button2.pack()
 
-        button3 = ttk.Button(self.window, text="Informes")
+        button3 = ttk.Button(button_frame, text="Informes")
         button3.pack()
 
         self.window.mainloop()
